ef539eb169c67d952fd6afe1ffe4a02b7ef9b45a
denemor <smnacakanoc@gmail.com>
2020-02-18 04:14:55 +0200

    Update. Добавлены переменные.
    1) Команды для очистки ОС.
    2) Список сетей для игнорирования.

--- agent/src/settings.py
+++ agent/src/settings.py
@@ -1,10 +1,17 @@
 import environs
-
+import os
 
 env = environs.Env()
 if env.bool("READ_ENV", False):
     env.read_env()
 
+if os.name == 'nt':
+    DEFAULT_CLEAN_COMMAND = "cleanmgr.exe /verylowdisk /sagerun:1"
+else:
+    DEFAULT_CLEAN_COMMAND = "yum clean all"
+
+IGNORED_NETWORKS = '127.0.0.0/8, 169.254.0.0/16'
+
 API_URL = env.str("API_URL", "http://localhost:5001/api/")
 
-CLEAN_COMMAND = "cleanmgr.exe /verylowdisk /sagerun:1"
+CLEAN_COMMAND = env.str("CLEAN_COMMAND", DEFAULT_CLEAN_COMMAND)
