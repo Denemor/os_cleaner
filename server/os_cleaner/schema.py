baea33c922b8f016872950d8f6e0bdd56117b727
denemor <smnacakanoc@gmail.com>
2020-02-18 04:01:02 +0200

    Update. Добавлена схема для отображения всех хостов и их тасков с данными о месте на диске.

--- server/os_cleaner/schema.py
+++ server/os_cleaner/schema.py
@@ -6,22 +6,21 @@ class QueryParamsSchema(Schema):
     limit = fields.Integer(default=20)
     offset = fields.Integer(default=0)
 
+    class Meta:
+        ordered = False
+
 
 class AgentSchema(Schema):
     id = fields.Integer()
     hostname = fields.String()
-    mac = fields.String()
     ip = fields.String()
 
-class DiskSchema(Schema):
+
+class DisksSchema(Schema):
     id = fields.Integer()
-    agent = AgentSchema()
+    agent_id = fields.String()
+    created_at = fields.DateTime()
     mountpoint = fields.String()
-
-
-class StatisticSchema(Schema):
-    id = fields.Integer()
-    disk = DiskSchema()
     total = fields.String()
     free = fields.String()
     used = fields.String()
@@ -29,12 +28,14 @@ class StatisticSchema(Schema):
 
 
 class TasksSchema(Schema):
-    agent = AgentSchema()
+    id = fields.String()
+    agent_id = fields.String()
+    created_at = fields.DateTime()
     code = fields.Int()
     output = fields.String()
     errors = fields.String()
+    command = fields.String()
 
-class AllSchema(Schema):
-    agent = fields.List(fields.Nested(AgentSchema))
-    tasks = fields.List(fields.Nested(TasksSchema))
-    disks = fields.List(fields.Nested(DiskSchema))
+class AllSchema(AgentSchema):
+    disks = fields.List(fields.Nested(DisksSchema()))
+    tasks = fields.List(fields.Nested(TasksSchema()))
