5731ceec94fcfedd1e0c060a5d2c11f43abc1a8d
denemor <smnacakanoc@gmail.com>
2020-02-18 04:03:24 +0200

    Update. Добавлены линки к таблице Agent.

--- server/os_cleaner/models.py
+++ server/os_cleaner/models.py
@@ -9,7 +9,7 @@ from sqlalchemy import (
     Float,
 )
 
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
 from .db import db
 
 
@@ -17,8 +17,8 @@ class BaseModel(db.Model):
     __abstract__ = True
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    created_at = Column(DateTime, default=datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
 
 
 class Agent(BaseModel):
@@ -30,10 +30,12 @@ class Agent(BaseModel):
 
     hostname = Column(String(length=250), default="")
     ip = Column(String(length=150))
-    mac = Column(String(length=25), default="")
+
+    disks = relationship("Disks", back_populates="agent")
+    tasks = relationship("Tasks", back_populates="agent")
 
 
-class Disk(BaseModel):
+class Disks(BaseModel):
     """
 
     """
@@ -41,25 +43,15 @@ class Disk(BaseModel):
     __tablename__ = "disks"
 
     agent_id = Column(Integer, ForeignKey(Agent.id), nullable=False)
-    agent = relationship(Agent, backref=backref("disks"))
+    agent = relationship(Agent, back_populates="disks")
 
     mountpoint = Column(String(length=300))
-
-
-class DiskStatistic(BaseModel):
-    """
-
-    """
-
-    __tablename__ = "statistics"
-
-    disk_id = Column(Integer, ForeignKey(Disk.id), nullable=False)
-    disk = relationship(Disk, backref=backref("statistics"))
 
     total = Column(Float)
     used = Column(Float)
     free = Column(Float)
     percent = Column(Float)
+
 
 class Tasks(BaseModel):
     """
@@ -69,9 +61,9 @@ class Tasks(BaseModel):
     __tablename__ = "tasks"
 
     agent_id = Column(Integer, ForeignKey(Agent.id), nullable=False)
-    agent = relationship(Agent, backref=backref("tasks"))
+    agent = relationship(Agent, back_populates="tasks")
 
     code = Column(Integer)
     output = Column(String(length=300))
     errors = Column(String(length=300))
-    executed_at = Column(DateTime, default=datetime.utcnow)
+    command = Column(String(length=700))
