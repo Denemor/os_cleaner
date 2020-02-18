413a61abc714f45b5d3b0129c41e2d78120af7e4
denemor <smnacakanoc@gmail.com>
2020-02-18 04:17:08 +0200

    Update. Убраны лишние импорты. Изменены методы работы с удалённым API.
    Дополнительно каждый вызов команды или отправка статистики будет так же передовать ip адресса и hostname хоста.
    Добавлен метод для вызова команд на хосте и отправке данных через API.

--- agent/src/app.py
+++ agent/src/app.py
@@ -1,29 +1,46 @@
 import logging
 import typing
+import subprocess
 from urllib.parse import urljoin
 
 import psutil
 import requests
 
-from src.settings import API_URL
-from src.utils import get_hostname, get_mac_addr, get_ip_addr
+from src.settings import API_URL, CLEAN_COMMAND
+from src.utils import get_hostname, get_ip_addr
 
 
 class Client:
-    login_url = urljoin(API_URL, "auth/login/")
+    login_url = urljoin(API_URL, "agent/")
     disk_usage_url = urljoin(API_URL, "disks/")
+    command_result_url = urljoin(API_URL, "tasks/")
 
     headers = {"Authorization": ""}
 
     def login(self):
-        data = {"hostname": get_hostname(), "mac": get_mac_addr(), "ip": get_ip_addr()}
-        resp = requests.post(self.login_url, json=data)
-        data = resp.json()
 
-        if resp.status_code == 200:
-            self.headers.update({"Authorization": data["access"]})
+        return {"hostname": get_hostname(),
+                "ip": get_ip_addr()}
 
-    def _get_disk_usage_data(self) -> typing.List[typing.Dict]:
+    def _command_execute(self) -> typing.Dict:
+        res = subprocess.Popen(CLEAN_COMMAND,
+                               stdout = subprocess.PIPE,
+                               stderr = subprocess.PIPE,
+                               )
+        stdout, stderr = res.communicate()
+
+        return {
+                "metadata": self.login(),
+                "data": {
+                         "code": res.returncode,
+                         "output": stdout.decode("utf-8"),
+                         "errors": stderr.decode("utf-8"),
+                         "command": CLEAN_COMMAND
+                        }
+                }
+
+
+    def _get_disk_usage_data(self):
         results = []
         for disk in psutil.disk_partitions():
             usage = psutil.disk_usage(disk.mountpoint)
@@ -36,18 +53,40 @@ class Client:
                     "percent": usage.percent,
                 }
             )
-        return results
+        return {
+                "metadata": self.login(),
+                "data": results
+                }
 
-    def send_disk_usage(self):
-        data = self._get_disk_usage_data()
-        resp = requests.post(self.disk_usage_url, json=data, headers=self.headers)
+    def send_data(self, func, url):
+        data = func()
 
-        if resp.status_code in {401, 403}:
-            self.login()
+        if not isinstance(data, list):
+            data = [data]
 
-        resp = requests.post(self.disk_usage_url, json=data, headers=self.headers)
+        resp = requests.post(url=url,
+                             json=data,
+                             headers=self.headers)
 
         if 200 <= resp.status_code < 300:
             logging.info(resp.status_code)
         else:
             logging.error(resp.text)
+
+    def run(self):
+        __ = [
+            {
+                'function': self._get_disk_usage_data,
+                'url': self.disk_usage_url
+            },
+            {
+                'function': self._command_execute,
+                'url': self.command_result_url
+            }
+        ]
+        for func in __:
+            try:
+                self.login()
+                self.send_data(func=func['function'], url=func['url'])
+            except Exception as e:
+                logging.error(e)
