e816cf6ae21cc180b3edd63a748757783e7e7207
denemor <smnacakanoc@gmail.com>
2020-02-18 04:13:30 +0200

    Update. Добавлены функции для получения IPv4 адрессов и hostname.

--- agent/src/utils.py
+++ agent/src/utils.py
@@ -1,22 +1,30 @@
 import psutil
 import socket
-import uuid
-import netifaces
 import ipaddress
+from agent.src.settings import IGNORED_NETWORKS
+
+def if_in_ignored_network(ip):
+    networks = IGNORED_NETWORKS.split(',')
+
+    for network in networks:
+        network = network.strip()
+        if type(ip) is not ipaddress.ip_address:
+            ip = ipaddress.ip_address(ip)
+
+        if ip in ipaddress.ip_network(network):
+            return True
+
+    return False
 
 def get_hostname() -> str:
     return socket.gethostname()
 
 
-def get_mac_addr() -> str:
-    mac = uuid.getnode()
-    return ':'.join(("%012X" % mac)[i:i+2] for i in range(0, 12, 2))
-
-
 def get_ip_addr() -> str:
-    return psutil.net_if_addrs()
-
-
-def network_addr():
-
-    netifaces.interfaces()
+    res = []
+    for __, data in psutil.net_if_addrs().items():
+        for info in data:
+            if info.family == socket.AF_INET:
+                if not if_in_ignored_network(info.address):
+                    res.append(info.address)
+    return ", ".join(res)
